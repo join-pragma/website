1032859ca61620bc6e81f4564fa68edf1d15ea34
tech@joinpragma.eu <tech@joinpragma.eu>
2025-07-06 00:17:28 +0100

    husky

--- eslint.config.js
+++ eslint.config.js
@@ -17,6 +17,9 @@ export default tseslint.config([
       reactRefresh.configs.vite,
       eslintConfigPrettier,
     ],
+    rules: {
+      'react-refresh/only-export-components': 'warn',
+    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
